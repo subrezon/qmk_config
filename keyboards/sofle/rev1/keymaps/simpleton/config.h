53fea051d9d825d2e0fc6b44398d411845efdfec
Daniel Osipishin <danielos@retarus.de>
2024-09-26 12:35:28 +0200

    config.h + remove readme

--- keyboards/sofle/rev1/keymaps/simpleton/config.h
+++ keyboards/sofle/rev1/keymaps/simpleton/config.h
@@ -1,8 +1,22 @@
-// Copyright 2024 Santosh Kumar (@santosh)
-// SPDX-License-Identifier: GPL-2.0-or-later
-
 #pragma once
 
-#define TRI_LAYER_LOWER_LAYER 2
-#define TRI_LAYER_UPPER_LAYER 3
-#define TRI_LAYER_ADJUST_LAYER 4
+// Latency
+#undef  DEBOUNCE
+#define DEBOUNCE 7
+#define USB_POLLING_INTERVAL_MS 1
+
+// Unicode
+#define UNICODE_SELECTED_MODES UNICODE_MODE_LINUX, UNICODE_MODE_WINCOMPOSE
+#define UNICODE_CYCLE_PERSIST false  // Don't remember the last used mode, always start with the first
+
+// Split
+#define EE_HANDS
+
+// Tap-Hold
+#undef  TAPPING_TERM
+#define TAPPING_TERM 225
+#define TAPPING_TERM_PER_KEY
+#define QUICK_TAP_TERM 120
+
+// debug usb
+#define SPLIT_WATCHDOG_ENABLE
