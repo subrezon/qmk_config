01f5236074635b9e59927f21e0b030d5d4e14a28
Daniel Osipishin <github@subrezon.xyz>
2024-12-20 19:52:10 +0100

    crkbd simpleton

---
+++ keyboards/crkbd/keymaps/simpleton/rules.mk
@@ -0,0 +1,19 @@
+CONVERT_TO = rp2040_ce
+
+# hardware
+OLED_ENABLE    = no
+OLED_DRIVER    = SSD1306
+ENCODER_ENABLE = no
+LTO_ENABLE     = yes
+
+# debounce
+DEBOUNCE_TYPE = asym_eager_defer_pk
+
+# keycodes
+EXTRAKEY_ENABLE     = yes
+MOUSEKEY_ENABLE     = yes
+UNICODEMAP_ENABLE   = yes
+
+# debug
+BOOTMAGIC_ENABLE = yes
+CONSOLE_ENABLE   = no
