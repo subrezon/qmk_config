a32359b544826eac875441c5862f7093dbedee1f
Daniel Osipishin <daniel@subrezon.de>
2026-03-31 21:25:22 +0200

    crkbd 3x5

---
+++ keyboards/crkbd/keymaps/simpleton-3x5/keymap.c
@@ -0,0 +1,222 @@
+#include QMK_KEYBOARD_H
+
+enum layers {
+    // alphas
+    COLEMAK = 0,
+    QWERTY,
+    
+    // layers
+    MEDIA,
+    NAVIGATION,
+    MOUSE,
+    FUNCTIONS,
+    NUMBERS,
+    SYMBOLS,
+
+    // template
+    TEMPLATE
+};
+
+// homerow mods (colemak)
+#define A_GUI   GUI_T(KC_A)
+#define R_ALT   ALT_T(KC_R)
+#define S_SHIFT SFT_T(KC_S)
+#define T_CTRL  CTL_T(KC_T)
+#define N_CTRL  CTL_T(KC_N)
+#define E_SHIFT SFT_T(KC_E)
+#define I_ALT   ALT_T(KC_I)
+#define O_GUI   GUI_T(KC_O)
+
+// homerow mods (qwerty)
+// #define A_GUI   GUI_T(KC_A)
+#define S_ALT    ALT_T(KC_S)
+#define D_SHIFT  SFT_T(KC_D)
+#define F_CTRL   CTL_T(KC_F)
+#define J_CTRL   CTL_T(KC_J)
+#define K_SHIFT  SFT_T(KC_K)
+#define L_ALT    ALT_T(KC_L)
+#define SCL_GUI  GUI_T(KC_SCLN)
+
+// thumb keys
+#define ESC_MEDI LT(MEDIA,      KC_ESC)
+#define SPC_NAVI LT(NAVIGATION, KC_SPACE)
+#define TAB_MOUS LT(MOUSE,      KC_TAB)
+#define ENT_FUNC LT(FUNCTIONS,  KC_ENTER)
+#define BSPC_NUM LT(NUMBERS,    KC_BACKSPACE)
+#define DEL_SYMB LT(SYMBOLS,    KC_DEL)
+
+// combos
+#define COPY  LCTL(KC_C)
+#define PASTE LCTL(KC_V)
+#define CUT   LCTL(KC_X)
+#define REDO  LCTL(KC_Y)
+#define UNDO  LCTL(KC_Z)
+
+// base layer switches
+#define DF_CLMK DF(COLEMAK)
+#define DF_QWER DF(QWERTY)
+
+// unicode
+enum unicode_names {
+    A_DIAERESIS_SMALL,
+    A_DIAERESIS_CAPITAL,
+    O_DIAERESIS_SMALL,
+    O_DIAERESIS_CAPITAL,
+    U_DIAERESIS_SMALL,
+    U_DIAERESIS_CAPITAL,
+    S_SHARP_SMALL,
+    S_SHARP_CAPITAL,
+    EURO_SIGN,
+};
+
+const uint32_t PROGMEM unicode_map[] = {
+    [A_DIAERESIS_SMALL]   = 0xe4,    // ä
+    [A_DIAERESIS_CAPITAL] = 0xc4,    // Ä
+    [O_DIAERESIS_SMALL]   = 0xf6,    // ö
+    [O_DIAERESIS_CAPITAL] = 0xd6,    // Ö
+    [U_DIAERESIS_SMALL]   = 0xfc,    // ü
+    [U_DIAERESIS_CAPITAL] = 0xdc,    // Ü
+    [S_SHARP_SMALL]       = 0xdf,    // ß
+    [S_SHARP_CAPITAL]     = 0x1e9e,  // ẞ
+    [EURO_SIGN]           = 0x20ac,  // €
+};
+
+#define MO_UML MO(UMLAUTS)
+#define UP_AD  UP(A_DIAERESIS_SMALL, A_DIAERESIS_CAPITAL)
+#define UP_OD  UP(O_DIAERESIS_SMALL, O_DIAERESIS_CAPITAL)
+#define UP_UD  UP(U_DIAERESIS_SMALL, U_DIAERESIS_CAPITAL)
+#define UP_SS  UP(S_SHARP_SMALL,     S_SHARP_CAPITAL)
+#define UM_EUR UM(EURO_SIGN)
+
+// keymap
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+    [ COLEMAK ] = LAYOUT_split_3x5_3_ex2(
+    //┌────────┬────────┬────────┬────────┬────────┬────────╥────────┬────────┬────────┬────────┬────────┬────────┐
+           KC_Q,    KC_W,    KC_F,    KC_P,    KC_B, XXXXXXX, XXXXXXX,    KC_J,    KC_L,    KC_U,    KC_Y, KC_QUOT,
+    //├────────┼────────┼────────┼────────┼────────┼────────╫────────┼────────┼────────┼────────┼────────┼────────┤
+          A_GUI,   R_ALT, S_SHIFT,  T_CTRL,    KC_G, XXXXXXX, XXXXXXX,    KC_M,  N_CTRL, E_SHIFT,   I_ALT,   O_GUI,
+    //├────────┼────────┼────────┼────────┼────────┼────────╨────────┼────────┼────────┼────────┼────────┼────────┤
+           KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                      KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH,
+    //└────────┴────────┴────────┼────────┼────────┼────────╥────────┼────────┼────────┼────────┴────────┴────────┘
+                                  ESC_MEDI,SPC_NAVI,TAB_MOUS,ENT_FUNC,BSPC_NUM,DEL_SYMB
+    //                           └────────┴────────╨────────╨────────┴────────┴────────┘
+    ),
+    [ QWERTY ] = LAYOUT_split_3x5_3_ex2(
+    //┌────────┬────────┬────────┬────────┬────────┬────────╥────────┬────────┬────────┬────────┬────────┬────────┐
+           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, KC_LBRC, KC_RBRC,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+    //├────────┼────────┼────────┼────────┼────────┼────────╫────────┼────────┼────────┼────────┼────────┼────────┤
+          A_GUI,   S_ALT, D_SHIFT,  F_CTRL,    KC_G, XXXXXXX, KC_QUOT,    KC_H,  J_CTRL, K_SHIFT,   L_ALT, SCL_GUI,
+    //├────────┼────────┼────────┼────────┼────────┼────────╨────────┼────────┼────────┼────────┼────────┼────────┤
+           KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,
+    //└────────┴────────┴────────┼────────┼────────┼────────╥────────┼────────┼────────┼────────┴────────┴────────┘
+                                  ESC_MEDI,SPC_NAVI,TAB_MOUS,ENT_FUNC,BSPC_NUM,DEL_SYMB
+    //                           └────────┴────────╨────────╨────────┴────────┴────────┘
+    ),
+    [ MEDIA ] = LAYOUT_split_3x5_3_ex2(
+    //┌────────┬────────┬────────┬────────┬────────┬────────╥────────┬────────┬────────┬────────┬────────┬────────┐
+        _______, UC_WINC,  UM_EUR, _______, _______, _______, _______, _______, UC_LINX, _______, _______, _______,
+    //├────────┼────────┼────────┼────────┼────────┼────────╫────────┼────────┼────────┼────────┼────────┼────────┤
+          UP_AD,   UP_OD,   UP_SS,   UP_UD, _______, _______, _______, RM_TOGG, KC_MPRV, KC_MNXT, KC_VOLD, KC_VOLU,
+    //├────────┼────────┼────────┼────────┼────────┼────────╨────────┼────────┼────────┼────────┼────────┼────────┤
+        _______, _______, _______, _______, _______,                   RM_NEXT, RM_HUEU, RM_VALU, RM_SPDU, RM_FLGN,
+    //└────────┴────────┴────────┼────────┼────────┼────────╥────────┼────────┼────────┼────────┴────────┴────────┘
+                                   _______, _______, _______, KC_MSTP, KC_MPLY, KC_MUTE
+    //                           └────────┴────────╨────────╨────────┴────────┴────────┘
+    ),
+    [ NAVIGATION ] = LAYOUT_split_3x5_3_ex2(
+    //┌────────┬────────┬────────┬────────┬────────┬────────╥────────┬────────┬────────┬────────┬────────┬────────┐
+        _______, _______, _______, _______, _______, _______, _______,    COPY, KC_PGUP,   KC_UP, KC_PGDN,    REDO,
+    //├────────┼────────┼────────┼────────┼────────┼────────╫────────┼────────┼────────┼────────┼────────┼────────┤
+        KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, _______, _______, _______,   PASTE, KC_LEFT, KC_DOWN, KC_RGHT, KC_CAPS,
+    //├────────┼────────┼────────┼────────┼────────┼────────╨────────┼────────┼────────┼────────┼────────┼────────┤
+        _______, _______, _______, _______, _______,                       CUT, KC_HOME,  KC_INS,  KC_END,    UNDO,
+    //└────────┴────────┴────────┼────────┼────────┼────────╥────────┼────────┼────────┼────────┴────────┴────────┘
+                                   _______, _______, _______,  KC_ENT, KC_BSPC,  KC_DEL
+    //                           └────────┴────────╨────────╨────────┴────────┴────────┘
+    ),
+    [ MOUSE ] = LAYOUT_split_3x5_3_ex2(
+    //┌────────┬────────┬────────┬────────┬────────┬────────╥────────┬────────┬────────┬────────┬────────┬────────┐
+        _______, _______, _______, _______, _______, _______, _______,    COPY, MS_WHLU,   MS_UP, MS_WHLD,    REDO,
+    //├────────┼────────┼────────┼────────┼────────┼────────╫────────┼────────┼────────┼────────┼────────┼────────┤
+        KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, _______, _______, _______,   PASTE, MS_LEFT, MS_DOWN, MS_RGHT, _______,
+    //├────────┼────────┼────────┼────────┼────────┼────────╨────────┼────────┼────────┼────────┼────────┼────────┤
+        _______, _______, _______, _______, _______,                       CUT, MS_WHLL, _______, MS_WHLR,    UNDO,
+    //└────────┴────────┴────────┼────────┼────────┼────────╥────────┼────────┼────────┼────────┴────────┴────────┘
+                                   _______, _______, _______, MS_BTN3, MS_BTN1, MS_BTN2
+    //                           └────────┴────────╨────────╨────────┴────────┴────────┘
+    ),
+    [ FUNCTIONS ] = LAYOUT_split_3x5_3_ex2(
+    //┌────────┬────────┬────────┬────────┬────────┬────────╥────────┬────────┬────────┬────────┬────────┬────────┐
+        KC_PSCR,   KC_F7,   KC_F8,   KC_F9,  KC_F12, _______, _______, _______, _______, _______, _______, _______,
+    //├────────┼────────┼────────┼────────┼────────┼────────╫────────┼────────┼────────┼────────┼────────┼────────┤
+        KC_SCRL,   KC_F4,   KC_F5,   KC_F6,  KC_F11, _______, _______, _______, DF_CLMK, DF_QWER, _______, _______,
+    //├────────┼────────┼────────┼────────┼────────┼────────╨────────┼────────┼────────┼────────┼────────┼────────┤
+        KC_PAUS,   KC_F1,   KC_F2,   KC_F3,  KC_F10,                   _______, _______, _______, _______, _______,
+    //└────────┴────────┴────────┼────────┼────────┼────────╥────────┼────────┼────────┼────────┴────────┴────────┘
+                                    KC_ESC,  KC_SPC,  KC_TAB, _______, _______, _______
+    //                           └────────┴────────╨────────╨────────┴────────┴────────┘
+    ),
+    [ NUMBERS ] = LAYOUT_split_3x5_3_ex2(
+    //┌────────┬────────┬────────┬────────┬────────┬────────╥────────┬────────┬────────┬────────┬────────┬────────┐
+        KC_LBRC,    KC_7,    KC_8,    KC_9, KC_RBRC, _______, _______, _______, _______, _______, _______, _______,
+    //├────────┼────────┼────────┼────────┼────────┼────────╫────────┼────────┼────────┼────────┼────────┼────────┤
+        KC_SCLN,    KC_4,    KC_5,    KC_6, KC_MINS, _______, _______, _______, KC_RCTL, KC_RSFT, KC_RALT, KC_RGUI,
+    //├────────┼────────┼────────┼────────┼────────┼────────╨────────┼────────┼────────┼────────┼────────┼────────┤
+         KC_GRV,    KC_1,    KC_2,    KC_3,  KC_EQL,                   _______, _______, _______, _______, _______,
+    //└────────┴────────┴────────┼────────┼────────┼────────╥────────┼────────┼────────┼────────┴────────┴────────┘
+                                   KC_BSLS,    KC_0, _______, _______, _______, _______
+    //                           └────────┴────────╨────────╨────────┴────────┴────────┘
+    ),
+    [ SYMBOLS ] = LAYOUT_split_3x5_3_ex2(
+    //┌────────┬────────┬────────┬────────┬────────┬────────╥────────┬────────┬────────┬────────┬────────┬────────┐
+        KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR, _______, _______, _______, _______, _______, _______, _______,
+    //├────────┼────────┼────────┼────────┼────────┼────────╫────────┼────────┼────────┼────────┼────────┼────────┤
+        KC_COLN,  KC_DLR, KC_PERC, KC_CIRC, KC_UNDS, _______, _______, _______, KC_RCTL, KC_RSFT, KC_RALT, KC_RGUI,
+    //├────────┼────────┼────────┼────────┼────────┼────────╨────────┼────────┼────────┼────────┼────────┼────────┤
+        KC_TILD, KC_EXLM,   KC_AT, KC_HASH, KC_PLUS,                   _______, _______, _______, _______, _______,
+    //└────────┴────────┴────────┼────────┼────────┼────────╥────────┼────────┼────────┼────────┴────────┴────────┘
+                                   KC_PIPE, KC_RPRN, _______, _______, _______, _______
+    //                           └────────┴────────╨────────╨────────┴────────┴────────┘
+    ),
+    [ TEMPLATE ] = LAYOUT_split_3x5_3_ex2(
+    //┌────────┬────────┬────────┬────────┬────────┬────────╥────────┬────────┬────────┬────────┬────────┬────────┐
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    //├────────┼────────┼────────┼────────┼────────┼────────╫────────┼────────┼────────┼────────┼────────┼────────┤
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    //├────────┼────────┼────────┼────────┼────────┼────────╨────────┼────────┼────────┼────────┼────────┼────────┤
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    //└────────┴────────┴────────┼────────┼────────┼────────╥────────┼────────┼────────┼────────┴────────┴────────┘
+                                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+    //                           └────────┴────────╨────────╨────────┴────────┴────────┘
+    )
+};
+
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case S_SHIFT:
+        case E_SHIFT:
+            return TAPPING_TERM;
+
+        case R_ALT:
+        case T_CTRL:
+        case N_CTRL:
+        case I_ALT:
+            return TAPPING_TERM + 50;
+
+        case A_GUI:
+        case O_GUI:
+            return TAPPING_TERM + 100;
+
+        case ESC_MEDI:
+        case SPC_NAVI:
+        case TAB_MOUS:
+        case ENT_FUNC:
+        case BSPC_NUM:
+        case DEL_SYMB:
+            return TAPPING_TERM;
+
+        default:
+            return TAPPING_TERM;
+    }
+}
